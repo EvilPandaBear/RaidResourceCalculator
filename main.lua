b05034097acc266c38795146a2d4d06176b88963
florian-redinger <florian.redinger@outlook.com>
2019-05-17 12:31:51 +0200

    FR: Changed Name and added Test Frame

--- main.lua
+++ main.lua
@@ -1,5 +1,21 @@
-SLASH_TEST1 = "/test1"
-SLASH_TEST2 = "/addontest1"
-SlashCmdList["TEST"] = function(msg)
-    message('HelloWorld')
+SLASH_RRC1 = "/rrc"
+SLASH_RRC2 = "/raidresourcecalculator"
+SlashCmdList["RRC"] = function(msg)
+    createFrame()
 end 
+local mainFrame
+function createFrame()
+    mainFrame = CreateFrame("Frame",nil, UIParent)
+    mainFrame:SetFrameStrata("BACKGROUND")
+    mainFrame:SetWidth(128) -- Set these to whatever height/width is needed 
+    mainFrame:SetHeight(64) -- for your Texture
+
+    local t = mainFrame:CreateTexture(nil,"BACKGROUND")
+    t:SetTexture("Interface\\Glues\\CharacterCreate\\UI-CharacterCreate-Factions.blp")
+    t:SetAllPoints(mainFrame)
+    mainFrame.texture = t
+
+    mainFrame:SetPoint("CENTER",0,0)
+    mainFrame:Show()
+
+end
