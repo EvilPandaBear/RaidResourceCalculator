a7a2a52fbb49d21f364ea10398eb129d5163a2e2
florian-redinger <florian.redinger@outlook.com>
2019-05-17 12:50:07 +0200

    FR: Added test frame

--- RaidResourceCalculator.lua
+++ RaidResourceCalculator.lua
@@ -1,9 +1,11 @@
 local AddonName, RaidResourceCalculator = ...
-
+local mainFrameVisible
+local mainFrame
 -- Possible console calls to open the addon
 SLASH_RAIDRESOURCECALCULATOR1 = '/rrc'
 SLASH_RAIDRESOURCECALCULATOR2 = '/rrcalculator'
 SLASH_RAIDRESOURCECALCULATOR2 = '/raidresourcecalc'
+SLASH_RAIDRESOURCECALCULATOR3 = '/raidresourcecalculator'
 
 -- Parse console calls and arguments
 function SlashCmdList.RAIDRESOURCECALCULATOR(cmd, editbox)
@@ -23,5 +25,26 @@ end
 -- ShowInterface is the entry point to the AddOns Interface
 -- TODO: add real interface
 function RaidResourceCalculator:ShowInterface()
-    message('Hello World')
+	if not mainFrameVisible then
+		createFrame()
+	else
+		mainFrame:Hide()
+		mainFrameVisible = false
+	end
+end
+
+function createFrame()
+    mainFrame = CreateFrame("Frame",nil,UIParent)
+	mainFrame:SetFrameStrata("BACKGROUND")
+	mainFrame:SetWidth(128) -- Set these to whatever height/width is needed 
+	mainFrame:SetHeight(64) -- for your Texture
+
+	local t = mainFrame:CreateTexture(nil,"BACKGROUND")
+	t:SetTexture("Interface\\Glues\\CharacterCreate\\UI-CharacterCreate-Factions.blp")
+	t:SetAllPoints(mainFrame)
+	mainFrame.texture = t
+
+	mainFrame:SetPoint("CENTER",0,0)
+	mainFrame:Show()
+	mainFrameVisible = true
 end
